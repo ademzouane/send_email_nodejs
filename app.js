e4df3708226fab06d358d82e771ce9e0bcb7f81a
ademzouane <zad1zad1990@gmail.com>
2023-10-21 19:15:14 +0100

    update app.js file

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 import express from "express"
 import dotenv from "dotenv"
 import {sendnodemailer} from "./controllers/sendemail.js"
-import sendgridemail from "./controllers/sendemail.js"
-import errorHandlerMiddleware from "./middelware/errorHandler.js"
-import notFound  from "./middelware/not-found.js"
+import {sendgridemail} from "./controllers/sendemail.js"
+import {errorHandlerMiddleware} from "./middelware/errorHandler.js"
+import {notFound}  from "./middelware/not-found.js"
 
 const app = express();
 dotenv.config();
 
-app.use("/sendemail/grid",sendgridemail);
-app.use("/sendemail",sendnodemailer)
+app.get("/sendemail/grid",sendgridemail);
+app.get("/sendemail",sendnodemailer);
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
 
 const PORT = process.env.PORT || 5000; 
-
-app.listen(Port ,()=>{ console.log("the server is listening to the PORT :  ", PORT).catch((err)=>{console.log(err)})});
+try{
+    app.listen(PORT ,()=>{ console.log(`the server is listening to the PORT : ${PORT} `)}) ;
+}catch(err)
+{(err)=>console.log(err)} 
